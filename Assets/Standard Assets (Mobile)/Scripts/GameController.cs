d7e58af683537787a0f72d69764eaafea3764505
Philipp <philipp.omenisch@gmail.com>
2014-01-25 14:59:59 +0100

    added biek

---
+++ Assets/Standard Assets (Mobile)/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject car;
+	public GameObject bike;
+	// Use this for initialization
+	void Start () {
+		bike.SetActive (false);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+						car.SetActive (false);
+						bike.SetActive (true);
+				}
+	
+	}
+}
