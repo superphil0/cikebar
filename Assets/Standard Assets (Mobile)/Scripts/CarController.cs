fa436fae19794276441ebb0203c4f5b735b85ba5
Philipp <philipp.omenisch@gmail.com>
2014-01-25 13:20:02 +0100

    add scrolling

---
+++ Assets/Standard Assets (Mobile)/Scripts/CarController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CarController : MonoBehaviour {
+	Vector3 newPos;
+	// Use this for initialization
+	void Start () {
+		bool isAndroid = (Application.platform == RuntimePlatform.Android);
+		Vector3 pos = new Vector3 (0, 0 ,0);
+		//this.gameObject.transform.position = pos;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount > 0)
+		{
+			foreach(Touch t in Input.touches)
+			{
+				newPos =this.gameObject.transform.position;
+				newPos.y = t.position.x / Screen.width;		
+				Debug.Log(t.position);
+				this.gameObject.transform.position = newPos;
+			}
+		}
+
+	}
+}
---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+// http://www.fraghalt.de/blog/unity3d-endless-runner-tutorial/
+public class BackgroundBehaviour : MonoBehaviour {
+	public Transform prefab;
+	private Vector2 position;		
+
+	private LinkedList<Transform> roads = new LinkedList<Transform>();
+	
+	private float posX = 0.0f;
+	private int numberOfRoads = 3;
+	// Use this for initialization
+	void Start () {
+		
+		// Init the scene with some road-pieces
+		for(int i=0;i < numberOfRoads;i++) {
+			Transform road = Instantiate(prefab) as Transform;
+			road.Translate(  posX,i * road.renderer.bounds.size.y, 0);
+			roads.AddLast(road);
+		}
+	}
+
+	// Use this for initialization
+	void Update () {
+		
+		Transform firstRoad = roads.First.Value;
+		Transform lastRoad = roads.Last.Value;
+		
+		// Create a new road if the first one is not 
+		// in sight anymore and destroy the first one
+		if(firstRoad.localPosition.y < -25f) {
+			roads.Remove(firstRoad);
+			Destroy(firstRoad.gameObject);
+			
+			Transform newRoad = Instantiate(prefab) as Transform;
+			newRoad.Translate(posX,lastRoad.localPosition.y + lastRoad.renderer.bounds.size.y ,0);
+		  roads.AddLast(newRoad);
+		  
+		  }
+		
+		// Move the available roads along the z-axis
+		foreach(Transform road in roads) {
+			road.Translate( 0f,-8f * Time.deltaTime,0f);      
+		}
+	}
+
+	
+
+}
